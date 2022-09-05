c4f053b077df8a63dcef81065b24c862045b5864
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2022-09-05 16:53:34 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://fxc7-api.herokuapp.com';
+var url = 'mongodb';
 try {
 if(url == 'https://fxc7-api.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
@@ -12,7 +12,7 @@ if(url == 'https://fxc7-api.herokuapp.com') throw console.log(color('Cek konfigu
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, ZhirrrGanss','green'))
+  console.log(color('Connected correctly to server, mongodb','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
